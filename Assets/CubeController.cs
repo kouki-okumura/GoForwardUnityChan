3c97a619946bfd5b947afec0a2364399f1650d8d
奥村公基 <okumura0329k@gmail.com>
2018-05-10 22:07:02 +0900

    Lesson7 課題
    一回目

---
+++ Assets/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+    private float speed = -0.2f;
+    private float deadLine = -10;
+    public AudioClip block;
+    AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = block;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(this.speed, 0, 0);
+        if(transform.position.x<this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("aiu");
+        if (other.gameObject.tag == "Cube" || other.gameObject.tag == "Ground")
+        {
+            audioSource.PlayOneShot(block);
+        }
+            
+
+        
+    }
+}
+
+
